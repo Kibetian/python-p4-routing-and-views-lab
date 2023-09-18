0d80eb0f13fb0641453e21b10f5d3d70f6796142
Kibetian <126007714+Kibetian@users.noreply.github.com>
2023-09-18 20:45:14 +0300

    done

--- server/app.py
+++ server/app.py
@@ -1,8 +1,42 @@
-#!/usr/bin/env python3
-
 from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return "<h1>Python Operations with Flask Routing and Views</h1>"
+
+@app.route('/print/<string:param>')
+def print_string(param):
+    print(param)  # Print the string to the console
+    return f"<p>Printed String: {param}</p>"
+
+@app.route('/count/<int:param>')
+def count(param):
+    numbers = '\n'.join(str(i) for i in range(1, param + 1))
+    return f"<p>Counting numbers from 1 to {param}:</p><pre>{numbers}</pre>"
+
+@app.route('/math/<float:num1><operation><float:num2>')
+def math(num1, operation, num2):
+    result = None
+    if operation == '+':
+        result = num1 + num2
+    elif operation == '-':
+        result = num1 - num2
+    elif operation == '*':
+        result = num1 * num2
+    elif operation == 'div':
+        if num2 != 0:
+            result = num1 / num2
+        else:
+            return "Division by zero is not allowed."
+    elif operation == '%':
+        result = num1 % num2
+
+    if result is not None:
+        return f"<p>{num1} {operation} {num2} = {result}</p>"
+    else:
+        return "Invalid operation."
+
 if __name__ == '__main__':
-    app.run(port=5555, debug=True)
+    app.run(debug=True)
